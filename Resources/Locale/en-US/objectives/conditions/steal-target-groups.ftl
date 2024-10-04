LaCumbiaDelCoronavirus/KS16

Localization of steal targets (#30153)
* Localization of steal targets

* Correction of localization of theft objects

* The second correction of the localization of theft targets

* Update steal-target-groups.ftl

Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>

* Revision of the localization method

* Choosing a simple option for localization

* Fix TechnologyDisk name

* Corrections based on feedback received

* correction of declension

---------

Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>

Resources/Locale/en-US/objectives/conditions/steal-target-groups.ftl
@@ -1,0 +1,66 @@
+# Traitor single items
+steal-target-groups-hypospray = hypospray
+steal-target-groups-handheld-crew-monitor = handheld crew monitor
+steal-target-groups-clothing-outer-hardsuit-rd = experimental research hardsuit
+steal-target-groups-hand-teleporter = hand teleporter
+steal-target-groups-clothing-shoes-boots-mag-adv = advanced magboots
+steal-target-groups-box-folder-qm-clipboard = requisition digi-board
+steal-target-groups-food-meat-corgi = prime-cut corgi meat
+steal-target-groups-captain-id-card = captain ID card
+steal-target-groups-jetpack-captain-filled = captain's jetpack
+steal-target-groups-weapon-antique-laser = antique laser pistol
+steal-target-groups-nuke-disk = nuclear authentication disk
+steal-target-groups-weapon-energy-shot-gun = energy shotgun
+
+# Thief Collection
+steal-target-groups-figurines = figurine
+steal-target-groups-heads-cloaks = head's cloak
+steal-target-groups-heads-bedsheets = head's bedsheet
+steal-target-groups-stamps = stamp
+steal-target-groups-door-remotes = door remote
+steal-target-groups-encryption-keys = encryption key
+steal-target-groups-technology-disks = technology disk
+steal-target-groups-id-cards = ID Card
+steal-target-groups-lamps = LAMP
+
+# Thief single item
+steal-target-groups-forensic-scanner = forensic scanner
+steal-target-groups-flippo-engraved-lighter = detective's Flippo engraved lighter
+steal-target-groups-ammo-tech-fab-circuitboard = ammo techfab circuit board
+steal-target-groups-clothing-head-hat-warden = warden's cap
+steal-target-groups-clothing-outer-hardsuit-void-paramed = paramedic void suit
+steal-target-groups-medical-tech-fab-circuitboard = medical techfab machine board
+steal-target-groups-clothing-headset-alt-medical = chief medical officer's over-ear headset
+steal-target-groups-research-and-development-server-machine-circuitboard = R&D server machine board
+steal-target-groups-fire-axe = fireaxe
+steal-target-groups-ame-part-flatpack =  AME flatpack
+steal-target-groups-salvage-expeditions-computer-circuitboard = salvage expeditions computer board
+steal-target-groups-cargo-shuttle-console-circuitboard = cargo shuttle console board
+steal-target-groups-clothing-eyes-hud-beer = beer goggles
+steal-target-groups-bible = bible
+steal-target-groups-clothing-neck-goldmedal = gold medal of crewmanship
+steal-target-groups-clothing-neck-clownmedal = clown medal
+
+# Thief structures
+steal-target-groups-teg = teg generator part
+steal-target-groups-freezer-heater = freezer or heater
+steal-target-groups-altar-nanotrasen = nanotrasen altar (any)
+
+steal-target-groups-nuclear-bomb = nuclear fission explosive
+steal-target-groups-fax-machine-captain = captain long range fax machine
+steal-target-groups-chem-dispenser = chemical dispenser
+steal-target-groups-xeno-artifact = alien artifact
+steal-target-groups-booze-dispenser = booze dispenser
+steal-target-groups-plant-rd = "RD's potted plant"
+steal-target-groups-toilet-golden-dirty-water = golden toilet
+
+# Thief Animal
+steal-target-groups-animal-named-cat = CMO's Cat
+
+steal-target-groups-animal-ian = Ian
+steal-target-groups-animal-mc-griff = McGriff
+steal-target-groups-animal-walter = Walter
+steal-target-groups-animal-morty = Morty
+steal-target-groups-animal-renault = Renault
+steal-target-groups-animal-shiva = Shiva
+steal-target-groups-animal-tropico = Tropico

A-Love-Letter-to-Infinity/A-Love-Letter-to-Infinity

Resources/Locale/en-US/objectives/conditions/steal-target-groups.ftl
@@ -1,0 +1,68 @@
+# Traitor single items
+steal-target-groups-hypospray = hypospray
+steal-target-groups-handheld-crew-monitor = handheld crew monitor
+steal-target-groups-clothing-outer-hardsuit-rd = experimental research hardsuit
+steal-target-groups-hand-teleporter = hand teleporter
+steal-target-groups-clothing-shoes-boots-mag-adv = advanced magboots
+steal-target-groups-box-folder-qm-clipboard = requisition digi-board
+steal-target-groups-food-meat-corgi = prime-cut corgi meat
+steal-target-groups-captain-id-card = captain ID card
+steal-target-groups-jetpack-captain-filled = captain's jetpack
+steal-target-groups-weapon-antique-laser = antique laser pistol
+steal-target-groups-nuke-disk = nuclear authentication disk
+steal-target-groups-weapon-energy-shot-gun = energy shotgun
+
+# Thief Collection
+steal-target-groups-figurines = figurine
+steal-target-groups-heads-cloaks = head's cloak
+steal-target-groups-heads-bedsheets = head's bedsheet
+steal-target-groups-stamps = stamp
+steal-target-groups-door-remotes = door remote
+steal-target-groups-encryption-keys = encryption key
+steal-target-groups-technology-disks = technology disk
+steal-target-groups-id-cards = ID Card
+steal-target-groups-lamps = LAMP
+
+# Thief single item
+steal-target-groups-forensic-scanner = forensic scanner
+steal-target-groups-flippo-engraved-lighter = detective's Flippo engraved lighter
+steal-target-groups-ammo-tech-fab-circuitboard = ammo techfab circuit board
+steal-target-groups-clothing-head-hat-warden = warden's cap
+steal-target-groups-clothing-outer-hardsuit-void-paramed = paramedic void suit
+steal-target-groups-medical-tech-fab-circuitboard = medical techfab machine board
+steal-target-groups-clothing-headset-alt-medical = chief medical officer's over-ear headset
+steal-target-groups-research-and-development-server-machine-circuitboard = R&D server machine board
+steal-target-groups-fire-axe = fireaxe
+steal-target-groups-ame-part-flatpack =  AME flatpack
+steal-target-groups-salvage-expeditions-computer-circuitboard = salvage expeditions computer board
+steal-target-groups-cargo-shuttle-console-circuitboard = cargo shuttle console board
+steal-target-groups-clothing-eyes-hud-beer = beer goggles
+steal-target-groups-bible = bible
+steal-target-groups-clothing-neck-goldmedal = gold medal of crewmanship
+steal-target-groups-clothing-neck-clownmedal = clown medal
+
+# Thief structures
+steal-target-groups-teg = teg generator part
+steal-target-groups-freezer-heater = freezer or heater
+steal-target-groups-altar-nanotrasen = nanotrasen altar (any)
+
+steal-target-groups-nuclear-bomb = nuclear fission explosive
+steal-target-groups-fax-machine-captain = captain long range fax machine
+steal-target-groups-chem-dispenser = chemical dispenser
+steal-target-groups-xeno-artifact = alien artifact
+steal-target-groups-booze-dispenser = booze dispenser
+# DeltaV - Epistemics Department - Replace RD with Mystagogue
+steal-target-groups-plant-rd = "Mytagogue's potted plant"
+steal-target-groups-toilet-golden-dirty-water = golden toilet
+
+# Thief Animal
+steal-target-groups-animal-named-cat = CMO's Cat
+
+steal-target-groups-animal-ian = Ian
+steal-target-groups-animal-mc-griff = McGriff
+steal-target-groups-animal-walter = Walter
+steal-target-groups-animal-morty = Morty
+steal-target-groups-animal-renault = Renault
+# DeltaV - Adjusts from Shiva because we have multiple possible sec animals
+steal-target-groups-animal-shiva = Security Pet
+steal-target-groups-animal-tropico = Tropico